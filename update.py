267c508399fff792de290b5498f601f84f9c44d4
Duane Bronson <Duane.Bronson@microfocus.com>
2020-08-03 12:30:31 -0400

    ran update
    fixed update.py to work for me

--- update.py
+++ update.py
@@ -20,7 +20,7 @@
 
 import csv
 import json
-import requests
+#import requests
 from datetime import datetime
 import numpy as np
 
@@ -203,7 +203,7 @@ for i in range(len(allWeekValues)):
          setColors.append(colors[2])
 
 
-with open('USStateColors.csv', 'w', newline='') as file:
+with open('USStateColors.csv', 'wb') as file:
     fieldnames = ['state', 'color']
     writer = csv.DictWriter(file, fieldnames=fieldnames)
     writer.writeheader()
